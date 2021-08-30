1a2989d066c9a89c52e78da34dcf145befe4b825
TanTran999 <tantan56787878@gmail.com>
2021-08-30 21:33:32 +0700

    go to welcome screen when firsr time open app

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: "/listLegend",
+      initialRoute: "/",
       routes: {
         "/": (context) => WelcomeScreen(),
         "/login": (context) => LoginScreen(),
